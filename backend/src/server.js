cce9653937f16c55843538bb886b0624df3ce2d7
Mortiz Ploss <moritz.ploss@appliedtechnology.se>
2019-11-15 13:34:32 +0100

    fix linting issues and missing dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ const PORT = (process.env.NODE_ENV === 'production')
   ? process.env.PROD_PORT
   : process.env.DEV_PORT;
 
+// eslint-disable-next-line no-console
 console.log(`start backend on port ${PORT} at ${new Date().toLocaleTimeString()}`);
 
 app.listen(PORT);
