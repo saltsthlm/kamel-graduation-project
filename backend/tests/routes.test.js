3b41b7815e63fcdef6dbba75ac6f19a4cb1f1084
Mortiz Ploss <moritz.ploss@appliedtechnology.se>
2019-11-15 14:16:57 +0100

    fix jest setup

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -2,10 +2,9 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('The home route', () => {
-  it('should respond to GET with status 200', () => {
-    request(app)
-      .get('/')
-      .set('Accept', 'text/html')
-      .expect(200);
+  it('should respond to GET with status 200', async (done) => {
+    const res = await request(app).get('/');
+    expect(res.status).toEqual(200);
+    done();
   });
 });
