90c15bd14f6af78b3e1cfe8727d5169bc560f94c
Mortiz Ploss <moritz.ploss@appliedtechnology.se>
2019-11-17 19:59:52 +0100

    refactoring + comments added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const expressWs = require('express-ws');
 
+// create an express-websocket instance that will wrap the express app
 const wsInstance = expressWs(express());
+// `app` can be used like in any other express app from here on
 const { app } = wsInstance;
 
-const getWsClients = (route) => wsInstance.getWss(route).clients;
+// helper function to format a string message, not so important
+const newClientRegistered = (clientId) => {
+  const time = new Date().toLocaleTimeString();
+  return JSON.stringify({
+    message: `client width id ${clientId} registered at ${time}`,
+  });
+};
 
+// this block will run every time a new socket connection is opened
+wsInstance.getWss().on('connection', (ws, req) => {
+  // send a welcome message to the client
+  ws.send(JSON.stringify({
+    message: `Welcome to Socket ${req.params.id}`,
+  }));
+  // send message to all connected clients saying that new client connected
+  const { clients } = wsInstance.getWss('/socket');
+  clients.forEach((client) => client.send(newClientRegistered(req.params.id)));
+});
+
+// this block will run every time data is sent through the socket connection,
+// execept for when the connection is opened (see above)
 app.ws('/socket/:id', (ws, req) => {
-  ws.on('message', () => {
-    ws.send(`Welcome to Socket ${req.params.id}`);
-
-    const clients = getWsClients(req.url);
-    const time = new Date().toLocaleTimeString();
-    clients.forEach((client) => client.send(`New client registered at ${time}`));
+  ws.on('message', (data) => {
+    console.log(JSON.parse(data));
   });
 });
 
--- frontend/index.js
+++ frontend/index.js
@@ -1,17 +1,37 @@
-// Set-up a peer-to-peer connection using WebRTC.
-// This will be used to exchange chat messages
+// set-up a websocket connection to our backend. this will be used to initialize 
+// the WebRTC connection (see below) later on
+
+// set-up a websocket connection to a random socket endpoint on our backend
+const clientId = Math.round(Math.random() * 2 ** 52);
+const socket = new WebSocket(`ws://localhost:8080/socket/${clientId}`);
+
+// when the socket connection opens, send a message to the server
+socket.onopen = () => {
+  socket.send(JSON.stringify({message: "Initialized connection on client!"})); 
+};
+
+// when we receive a message from the server, print it to the console
+socket.onmessage = (event) => console.log(event.data);
+
+
+
+
+
+// set-up a peer-to-peer connection using WebRTC.
+// this will be used to exchange chat messages
 
 const Peer = require('simple-peer');
 
 // create a new Peer
 const peer = new Peer({
-  initiator: location.hash === '#1',
+  initiator: location.hash === '#init',
   trickle: false,
 })
 
 // after signaling has succeeeded, print signaling meta data to page
 peer.on('signal', (data) => {
   document.getElementById('yourId').innerText = JSON.stringify(data);
+  socket.send(JSON.stringify({clientId, data})); 
 })
 
 // establish connection to other peer
@@ -30,18 +50,3 @@ document.getElementById('send').addEventListener('click', () => {
 peer.on('data', (data) => {
   document.getElementById('messages').textContent += `${data}\n`;
 })
-
-
-// Set-up a websocket connection.
-// This wil be used to initialize the WebRTC connection (see above)
-
-// Set-up a websocket connection to our backend
-const socket = new WebSocket('ws://localhost:8080/socket/123');
-
-// when the socket connection opens, send a message to the server
-socket.onopen = () => {
-  socket.send("Initialized connection on client!"); 
-};
-
-// when we receive a message from the server, print it to the console
-socket.onmessage = (event) => console.log(event.data);
