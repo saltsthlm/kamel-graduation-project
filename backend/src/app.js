26b2e94e70d140ab6f62a26f41792f0fb46688e6
Mortiz Ploss <moritz.ploss@appliedtechnology.se>
2019-11-19 09:09:27 +0100

    latest changes to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,39 +1,41 @@
 const express = require('express');
 const expressWs = require('express-ws');
 
-// create an express-websocket instance that will wrap the express app
 const wsInstance = expressWs(express());
-// `app` can be used like in any other express app from here on
 const { app } = wsInstance;
 
-// helper function to format a string message, not so important
-const newClientRegistered = (clientId) => {
-  const time = new Date().toLocaleTimeString();
-  return JSON.stringify({
-    message: `client width id ${clientId} registered at ${time}`,
-  });
+const connectedClients = [];
+
+const addConnectedClient = (clientId) => {
+  connectedClients.push(clientId);
 };
 
-// this block will run every time a new socket connection is opened
-wsInstance.getWss().on('connection', (ws, req) => {
-  // send a welcome message to the client
-  ws.send(JSON.stringify({
-    message: `Welcome to Socket ${req.params.id}`,
-  }));
-  // send message to all connected clients saying that new client connected
+const removeConnectedClient = (clientId) => {
+  connectedClients.splice(connectedClients.indexOf(clientId), 1);
+};
+
+const communicateConnectedClients = () => {
   const { clients } = wsInstance.getWss('/socket');
-  clients.forEach((client) => client.send(newClientRegistered(req.params.id)));
+  clients.forEach((client) => client.send(JSON.stringify({ connectedClients })));
+};
+
+wsInstance.getWss().on('connection', (ws, req) => {
+  req.id = req.params.id;
+  addConnectedClient(req.params.id);
+  communicateConnectedClients();
 });
 
-// this block will run every time data is sent through the socket connection,
-// execept for when the connection is opened (see above)
-app.ws('/socket/:id', (ws, req) => {
+app.ws('/socket/:id', (ws) => {
   ws.on('message', (data) => {
     console.log(JSON.parse(data));
   });
+
+  ws.on('close', (req) => {
+    removeConnectedClient(req.id);
+    communicateConnectedClients();
+  });
 });
 
-// just to make our sample unit test pass
 app.get('/', (req, res) => res.send('Hi'));
 
 module.exports = app;
