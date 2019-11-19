10ed23671aea68a60254a1fe15efa873366b275b
SaltErik <erik.nygren@appliedtechnology.se>
2019-11-19 10:54:51 +0100

    fix chat and refactor

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,29 +4,44 @@ const expressWs = require('express-ws');
 const wsInstance = expressWs(express());
 const { app } = wsInstance;
 
-const connectedClients = [];
+let connectedClients = [];
 
-const addConnectedClient = (clientId) => {
-  connectedClients.push(clientId);
+const addConnectedClient = (clientId, ws) => {
+  connectedClients.push({
+    clientId,
+    ws,
+  });
 };
 
 const removeConnectedClient = (clientId) => {
-  connectedClients.splice(connectedClients.indexOf(clientId), 1);
+  connectedClients = connectedClients.filter((client) => client.clientId !== clientId);
+};
+
+const deliverParcel = (parcel) => {
+  const { ws } = connectedClients.find((client) => client.clientId === parcel.receiverId);
+  ws.send(JSON.stringify(parcel));
 };
 
 const communicateConnectedClients = () => {
-  const { clients } = wsInstance.getWss('/socket');
-  clients.forEach((client) => client.send(JSON.stringify({ connectedClients })));
+  const parcel = {
+    type: 'UPDATE CONTACTLIST',
+    connectedClients: connectedClients.map((client) => client.clientId),
+  };
+  connectedClients.forEach((client) => deliverParcel({ ...parcel, receiverId: client.clientId }));
 };
 
 wsInstance.getWss().on('connection', (ws, req) => {
   req.id = req.params.id;
-  addConnectedClient(req.params.id);
+  addConnectedClient(req.id, ws);
   communicateConnectedClients();
 });
 
 app.ws('/socket/:id', (ws) => {
   ws.on('message', (data) => {
+    const parcel = JSON.parse(data);
+    if (parcel.type === 'DIRECT MESSAGE') {
+      deliverParcel(parcel);
+    }
     console.log(JSON.parse(data));
   });
 
--- client/src/components/ContactList.jsx
+++ client/src/components/ContactList.jsx
@@ -1,15 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-function ContactList({ contactList }) {
-  const establishConnection = (event) => {
-    console.log(event.target.innerText)
-  }
+function ContactList({ contactList, sendMessage }) {
+  const establishConnection = event => {
+    sendMessage(event.target.innerText, "Hello");
+  };
 
   return (
     <div className="contact-list">
       <h3>Active Users</h3>
       <ul>
-        {contactList.map((contact, i) => <li key={i} onClick={establishConnection}>{contact}</li>)}
+        {contactList.map((contact, i) => (
+          <li key={i} onClick={establishConnection}>
+            {contact}
+          </li>
+        ))}
       </ul>
     </div>
   );
