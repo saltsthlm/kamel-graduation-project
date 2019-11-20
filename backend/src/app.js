94e2580716e0423d5c1c8d4f234136d101728cb8
Mortiz Ploss <moritz.ploss@appliedtechnology.se>
2019-11-20 21:44:44 +0100

    add basic logging setup with Morgan/Winston to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const expressWs = require('express-ws');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const sockets = require('./controllers/sockets');
+const logger = require('./logging/logging');
 
 const wsInstance = expressWs(express());
 const { app } = wsInstance;
+
+app.use(helmet());
+app.use(morgan('tiny', { stream: logger.stream }));
 
 wsInstance.getWss().on('connection', sockets.onConnect);
 
@@ -14,5 +20,12 @@ app.ws('/socket/:id', (ws, req) => {
 });
 
 app.get('/', (req, res) => res.send('Hi'));
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  logger.logger.error(err);
+  req.session.spotifyCode = undefined;
+  res.status(500).send('An Error Occured');
+});
 
 module.exports = app;
--- backend/src/controllers/sockets.js
+++ backend/src/controllers/sockets.js
@@ -1,22 +1,29 @@
 const client = require('../clients/clients');
 const { translate } = require('../translate/translation');
+const { logger } = require('../logging/logging');
 
 const onConnect = (ws, req) => {
   req.id = req.params.id;
   client.addConnected(req.params.id, ws);
   client.communicateConnected();
+  logger.info(`opened connection on socket ${req.id}`);
 };
 
 const onClose = (req) => {
   client.removeConnected(req.id);
   client.communicateConnected();
+  logger.info(`closed connection on socket ${req.id}`);
 };
 
-const onMessage = async (data) => {
-  const parcel = JSON.parse(data);
+// deliberate abstraction for future implementation
+const getTargetLanguage = (receiverId) => 'sv';
 
+const processParcel = async (parcel) => {
   if (parcel.type === 'DIRECT MESSAGE') {
-    const translatedMessage = await translate(parcel.message, 'sv');
+    const translatedMessage = await translate(
+      parcel.message,
+      getTargetLanguage(parcel.receiverId),
+    );
     client.deliverParcel({ ...parcel, translatedMessage });
   }
 
@@ -26,8 +33,16 @@ const onMessage = async (data) => {
       receiverId: parcel.senderId,
     });
   }
+}
 
-  console.log(JSON.parse(data));
+const onMessage = async (data) => {
+  const parcel = JSON.parse(data);
+  if (parcel.type) {
+    logger.info(`received parcel of type ${parcel.type}`);
+    await processParcel(parcel);
+    logger.info(parcel);
+    logger.info(`delivered parcel of type ${parcel.type}`);
+  }
 };
 
 module.exports = {
