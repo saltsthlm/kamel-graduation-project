a054593d9624a48a1d1023eab2ce40770c938752
SaltErik <erik.nygren@appliedtechnology.se>
2019-11-20 22:37:22 +0100

    refactor tests

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -1,10 +1,12 @@
+`use strict`;
 const request = require('supertest');
 const app = require('../src/app');
 
 describe('The home route', () => {
-  it('should respond to GET with status 200', async (done) => {
-    const res = await request(app).get('/');
-    expect(res.status).toEqual(200);
-    done();
+  it('should respond to GET with status 200', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(done);
   });
 });
