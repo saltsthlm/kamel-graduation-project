9316497d82caafd2e962df2cadbc27ddfbdb0ed5
SaltErik <erik.nygren@appliedtechnology.se>
2019-11-23 16:45:51 +0100

    ugly attempt at fixing some dev machine inconsistencies

--- init.sh
+++ init.sh
@@ -1,11 +1,27 @@
-brew update
-brew upgrade yarn
-brew upgrade node
-brew install kompose
-brew install minikube
-docker pull mongo
+rm -f ./init_errors.log
+touch ./init_errors.log
 
-gcloud components install kubectl
+brew update 2>>init_errors.log
+
+brew install node 2>>init_errors.log
+brew upgrade node 2>>init_errors.log
+
+brew install yarn 2>>init_errors.log
+brew upgrade yarn 2>>init_errors.log
+
+brew install kompose 2>>init_errors.log
+brew upgrade kompose 2>>init_errors.log
+
+brew install minikube 2>>init_errors.log
+brew upgrade minikube 2>>init_errors.log
+
+brew install docker 2>>init_errors.log
+brew upgrade docker 2>>init_errors.log
+brew link docker 2>>init_errors.log
+
+docker pull mongo 2>>init_errors.log
+
+gcloud components install kubectl 2>>init_errors.log
 
 cd backend
 yarn install
@@ -14,4 +30,3 @@ cd ..
 cd client
 yarn install
 cd ..
-
