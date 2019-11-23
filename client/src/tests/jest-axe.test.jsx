4dccc9ce74d3f7e5e75138b0dcfe4ba0b7da5942
SaltErik <erik.nygren@appliedtechnology.se>
2019-11-23 17:06:38 +0100

    added jest-axe plus initial test

---
+++ client/src/tests/jest-axe.test.jsx
@@ -0,0 +1,14 @@
+`ues strict`;
+const React = require("react");
+const App = require("./../App").default;
+const { render, cleanup } = require("@testing-library/react");
+const { axe, toHaveNoViolations } = require("jest-axe");
+
+expect.extend(toHaveNoViolations);
+
+it("should demonstrate this matcher`s usage with react testing library", async () => {
+  const { container } = render(<App />);
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+  cleanup();
+});
