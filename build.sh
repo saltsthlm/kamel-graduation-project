5623d41843e12f2b260be53299df7a5c7c4d0163
Mortiz Ploss <moritz.ploss@appliedtechnology.se>
2019-11-24 09:48:22 +0100

    remove google github workflow

--- build.sh
+++ build.sh
@@ -1,25 +1,12 @@
 cd client
+yarn test
 yarn build
 cd ..
 
-# docker-compose  up --build
+cd backend
+yarn test
+cd ..
 
 gcloud config set project my-project-1574063061768
 gcloud config set compute/zone europe-west3-a
 gcloud builds submit --config cloudbuild.yml .
-
-
-# kompose convert
-# kubectl apply -f backend-deployment.yaml, database-claim0-persistentvolumeclaim.yaml, database-deployment.yaml, frontend-claim0-persistentvolumeclaim.yaml, frontend-deployment.yaml, frontend-service.yaml
-# minikube service frontend
-
-# gcloud config set project my-project-1574063061768
-# gcloud config set compute/zone europe-west3-a
-# gcloud config set container/cluster kamel
-# gcloud container clusters get-credentials kamel --zone europe-west3-a --project my-project-1574063061768
-# kubectl proxy
-
-# gcloud docker -- push 
-
-# gcloud container clusters get-credentials kamel
-# kubectl create deployment kamel-server --image=
